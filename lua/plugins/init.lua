4ba8b8d21459bdc6acfe0024a92ab5bbea38c44c
tlonny <tlonny@lonnycore.lan>
2024-10-11 16:29:47 +0100

    add bufferline back

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,13 @@
 require("util/bootstrap")
 
 require("lazy").setup({
-    require("plugins/cmp"),
-    require("plugins/nvim-jdtls"),
+    require("plugins/bufferline"),
     require("plugins/catppuccin"),
+    require("plugins/cmp"),
     require("plugins/lspconfig"),
     require("plugins/lualine"),
     require("plugins/mason"),
+    require("plugins/nvim-jdtls"),
     require("plugins/nvimtree"),
     require("plugins/telescope"),
     require("plugins/treesitter"),
